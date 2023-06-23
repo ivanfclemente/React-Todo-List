787611f977b9662f9cf99bf0bdab7907560b8b93
pai-natal <ivanfclemente@gmail.com>
2023-06-23 08:58:57 +0100

    main page components

---
+++ src/components/AddToList.jsx
@@ -0,0 +1,20 @@
+import classes from "./AddToList.module.css";
+import { useRef } from "react";
+
+const AddToList = (props) => {
+  const inputRef = useRef();
+
+  return (
+    <>
+      <input type="text" className={classes.textBox} ref={inputRef} />
+      <button
+        className={classes.button}
+        onClick={() => props.onAddItem(inputRef.current.value)}
+      >
+        Add item
+      </button>
+    </>
+  );
+};
+
+export default AddToList;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Card from "../components/Card";
+import AddToList from "../components/AddToList";
+import List from "../components/List";
 
 const MainPage = () => {
+  const [toDoItems, setToDoItems] = useState([
+    "Learn React",
+    "Finish coding sprint",
+  ]);
+
+  const addItemHandler = (item) => {
+    setToDoItems((prev) => prev.concat(item));
+    console.log(toDoItems);
+  };
+
   return (
     <>
       <h1>Main</h1>
+
       <Link to="history">History</Link>
+      <Card>
+        <AddToList onAddItem={addItemHandler} />
+        <List toDoItems={toDoItems} />
+      </Card>
     </>
   );
 };
